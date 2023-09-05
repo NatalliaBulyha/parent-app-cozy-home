5e8c7ae92f9ce9024061e060655c99012fc26caa
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-05 17:17:39 +0200

    add changes

---
+++ review-service/src/main/java/com/cozyhome/onlineshop/reviewservice/config/YsqlConfig.java
@@ -0,0 +1,28 @@
+package com.cozyhome.onlineshop.reviewservice.config;
+
+import javax.sql.DataSource;
+
+import com.cozyhome.onlineshop.reviewservice.repository.ReviewRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.TransactionManager;
+
+import com.yugabyte.data.jdbc.datasource.YugabyteTransactionManager;
+import com.yugabyte.data.jdbc.repository.config.AbstractYugabyteJdbcConfiguration;
+import com.yugabyte.data.jdbc.repository.config.EnableYsqlRepositories;
+
+@Configuration
+@EnableYsqlRepositories(basePackageClasses = ReviewRepository.class)
+public class YsqlConfig extends AbstractYugabyteJdbcConfiguration {
+    @Bean
+    NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    TransactionManager transactionManager(DataSource dataSource) {
+        return new YugabyteTransactionManager(dataSource);
+    }
+}
