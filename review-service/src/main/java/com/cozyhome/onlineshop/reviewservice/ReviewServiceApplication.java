67b916e365894c1636dcad5276e8e26db942d6f3
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-05 18:34:52 +0200

    add changes

--- review-service/src/main/java/com/cozyhome/onlineshop/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/cozyhome/onlineshop/reviewservice/ReviewServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"com.cozyhome.onlineshop.reviewservice.model"})
 @EnableJpaRepositories(basePackages = {"com.cozyhome.onlineshop.reviewservice.repository"})
 @SpringBootApplication
 public class ReviewServiceApplication {
