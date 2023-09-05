bc9c47f51a979ef982e0cdb116ab18a03a853222
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-05 20:26:31 +0200

    add changes

--- review-service/src/main/java/com/cozyhome/onlineshop/reviewservice/controller/ReviewControllerImpl.java
+++ review-service/src/main/java/com/cozyhome/onlineshop/reviewservice/controller/ReviewControllerImpl.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("${api.basePath}/review")
+@RequestMapping("review")
 public class ReviewControllerImpl {
 
     private final ReviewService reviewService;
